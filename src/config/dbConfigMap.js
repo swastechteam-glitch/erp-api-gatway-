1519bae494f116db3e1464e8a3053e1717bbda66
anbukathirswas <anbukathirswas@gmail.com>
2026-06-23 11:55:14 +0530

    database changes

--- src/config/dbConfigMap.js
+++ src/config/dbConfigMap.js
@@ -191,10 +191,9 @@ export const clientDBConfig = {
   DEV: {
     user: "sa",
     password: "@dmin1305",
-    server: "117.200.77.84",
+    server: "103.208.228.199",
     port: 16000,
-    database: "SwasERP_KAS_Check",
-    // database: "SwasERP_KAS",
+    database: "SwasERP_SKT_Test",
   },
   KAS: {
     user: "sa",
